9b1c85e879e213ffc9150b27d80d3c6bfecf2196
Christopher Metzger <chrismetzgr@gmail.com>
2018-04-15 22:05:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,4 +7,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 function removeFromPlaylist(playlist, artistName) {
    delete playlist[artistName];
    return playlist
+function updatePlaylist(playlist, artistName, songTitle) { return Object.assign({}, playlist, { [artistName]:songTitle });
+}
 }
